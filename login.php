c5a5729b0dd302df82977c2052c5233d557ef711
jolo20 <dxjolo@gmail.com>
2026-02-13 16:56:24 +0800

    g

--- login.php
+++ login.php
@@ -140,9 +140,8 @@ if (!$isDefinitelyLocalhost && $finalHost !== '') {
             display: flex;
             align-items: center;
             justify-content: center;
-            padding: 20px 24px;
+            padding: 40px 24px;
             position: relative;
-            min-height: calc(100vh - 140px);
         }
 
         .login-main::before {
