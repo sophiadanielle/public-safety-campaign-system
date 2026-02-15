6ce8ec23efe95ad94b4abb9fd2e2ddec4a18bf7d
jolo20 <dxjolo@gmail.com>
2026-02-15 22:59:02 +0800

    g

--- public/campaigns.php
+++ public/campaigns.php
@@ -134,7 +134,7 @@ require_once __DIR__ . '/../sidebar/includes/block_viewer_access.php';
         <div class="campaign-page">
 <style>
                 html, body {
-            margin: 13px;
+            margin: 25px;
             padding: 0;
         }
 
