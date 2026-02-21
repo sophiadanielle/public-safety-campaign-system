e67cec678b4b6eb4b3485c52e57e6f7d705ea6e5
jolo20 <dxjolo@gmail.com>
2026-02-21 20:01:21 +0800

    g

--- public/user_management.php
+++ public/user_management.php
@@ -8,6 +8,9 @@ require_once __DIR__ . '/../sidebar/includes/block_viewer_access.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title><?php echo htmlspecialchars($pageTitle); ?> - Public Safety Campaign</title>
     <script>
         (function () {
